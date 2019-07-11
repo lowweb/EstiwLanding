6ac4b9268a823dc0210629adcfe74fd5aaa7487b
lowweb <none>
2019-07-11 16:24:49 +1000

    2step mobile

--- src/blocks/modules/header/header.js
+++ src/blocks/modules/header/header.js
@@ -3,7 +3,9 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 670 || document.documentElement.scrollTop > 670) {
     document.getElementsByClassName('hd')[0].classList.add('hd--fix');
+    document.getElementsByClassName('hd')[0].classList.remove('hd--tr');
   } else {
     document.getElementsByClassName('hd')[0].classList.remove('hd--fix');
+    document.getElementsByClassName('hd')[0].classList.add('hd--tr');
   }
 }
