010f769ee90c73e651fd9beeeae1c37d3ce5239f
lowweb <none>
2019-07-23 16:30:08 +1000

    fix img webp

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -75,10 +75,10 @@ requireDir("./gulp-tasks/");
 export { paths };
 
 export const development = gulp.series("clean", "smart-grid",
-    gulp.parallel(["views", "styles", "scripts", "images", /*"webp",*/ "sprites", "fonts", "favicons"]),
+    gulp.parallel(["views", "styles", "scripts", "images", "webp", "sprites", "fonts", "favicons"]),
     gulp.parallel("serve"));
 
 export const prod = gulp.series("clean",
-    gulp.series(["views", "styles", "scripts", "images", /*"webp"*/ "sprites", "fonts", "favicons", "gzip"]));
+    gulp.series(["views", "styles", "scripts", "images", "webp", "sprites", "fonts", "favicons", "gzip"]));
 
 export default development;
