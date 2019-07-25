56452dd14dd40f61183f12fa657e4b9026a5b94a
lowweb <none>
2019-07-25 13:33:16 +1000

    chng project

--- gulp-tasks/styles.js
+++ gulp-tasks/styles.js
@@ -22,7 +22,7 @@ gulp.task("styles", () => {
         .pipe(gulpif(!production, sourcemaps.init()))
         .pipe(plumber())
         .pipe(sass())
-        .pipe(groupmedia())
+        .pipe(gulpif(production, groupmedia()))
         .pipe(gulpif(production, autoprefixer({
             cascade: false,
             grid: true
