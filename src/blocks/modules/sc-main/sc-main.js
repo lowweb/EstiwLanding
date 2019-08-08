d755ffe7b4e6bccdb81ff6663e06520224cd35df
lowweb <lowweb>
2019-08-09 00:34:33 +1000

    video back

--- src/blocks/modules/sc-main/sc-main.js
+++ src/blocks/modules/sc-main/sc-main.js
@@ -2,21 +2,27 @@ document.getElementById("btn-scroll").onclick = function() {
 	document.getElementById('scrl-anch').scrollIntoView({block: 'start',behavior: 'smooth' });
 };
 
-var vid = document.getElementById("bgvid");
 
-if (window.matchMedia('(prefers-reduced-motion)').matches) {
-    vid.removeAttribute("autoplay");
-    vid.pause();
+if (window.matchMedia("(min-width: 767px)").matches) {
+	var videoCont = document.getElementById('videoCont');
+	var gradElm = document.createElement('div');
+	var videoElm = document.createElement('video');
+
+	gradElm.setAttribute('class', 'sc-main__grad');
+
+	videoElm.setAttribute('autoplay', 'autoplay');
+	videoElm.setAttribute('loop', 'loop');
+	videoElm.setAttribute('preload', 'auto');
+	videoElm.setAttribute('muted', 'muted');
+	videoElm.setAttribute('class', 'sc-main__video');
+	// videoElm.setAttribute('poster', 'img/hd-wd.jpg');
+
+	var mp4Source = document.createElement('source');
+	mp4Source.setAttribute('src', 'img/sc-main-video.mp4');
+	mp4Source.setAttribute('type', 'video/mp4');
+	videoElm.appendChild(mp4Source);
+
+	videoCont.style.backgroundColor = '#00171F';
+	videoCont.appendChild(gradElm);
+	videoCont.appendChild(videoElm);
 }
-
-function vidFade() {
-  vid.classList.add("stopfade");
-}
-
-vid.addEventListener('ended', function()
-{
-// only functional if "loop" is removed 
-vid.pause();
-// to capture IE10
-vidFade();
-}); 
